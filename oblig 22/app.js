7a1348923b652a79b16937e1b5c08bd1116bd1ae
jorgefkNtnu <150465227+jorgefkNtnu@users.noreply.github.com>
2024-08-29 20:42:24 +0200

    Delete oblig 22 directory
    skal bytte navn

--- oblig 22/app.js
+++
@@ -1,9 +0,0 @@
-export function isLeapYear(year) {
-  if (year === undefined || year === null)
-    throw new Error("Invalid argument: year can not be undefined or null.");
-  if (year < 0)
-    throw new Error(
-      "Invalid argument: year must be an integer equal to or larger than 0."
-    );
-  return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
-}
